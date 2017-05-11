7ddb24aec3fa1aabd4868166168b07e5718069c4
Tomas Zahradnik <tomas.zahradnik@kiwi.com>
2017-05-11 22:52:28 +0200

    avada kedavra

--- chef/kiwi_bistro.py
+++ chef/kiwi_bistro.py
@@ -1,7 +1,11 @@
 import requests
+import io
+
+from google.cloud import vision
+
 
 #tmp hardcoded cookies
-headers={"cookie":"__inf_etc__=07d36902-30bb-4b12-a08e-183edb350ea5; forterToken=04123105931; __utma=96175366.2023676527.1466634696.1480537433.1490355071.3; __utmz=96175366.1490355071.3.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); rCookie=yg8mcyicjna97gk2yyacmbffnv94wg; _ga=GA1.2.2023676527.1466634696; ab_version=B; JSESSIONID=EB2237B25DFDD2B69F820695C3DEC7D8"})
+headers={"cookie":"__inf_etc__=07d36902-30bb-4b12-a08e-183edb350ea5; forterToken=04123105931; __utma=96175366.2023676527.1466634696.1480537433.1490355071.3; __utmz=96175366.1490355071.3.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); rCookie=yg8mcyicjna97gk2yyacmbffnv94wg; _ga=GA1.2.2023676527.1466634696; ab_version=B; JSESSIONID=EB2237B25DFDD2B69F820695C3DEC7D8"}
 menu_en="https://confluence.kiwi.com/download/attachments/12485782/9.5.-12.5.ENGL.jpg?version=1&modificationDate=1494313296287&api=v2&download=true"
 menu_cz="https://confluence.kiwi.com/download/attachments/12485782/9.5.-12.5.CZ.jpg?version=1&modificationDate=1494336129429&api=v2&download=true"
 
@@ -12,3 +16,23 @@ def get_en_menu():
 def get_cz_menu():
     r_cz = requests.get(menu_cz, headers=headers)
     return r_cz.content
+
+def text_from_image(raw_image):
+    vision_client = vision.Client()
+    with io.BytesIO(raw_image) as image_file:
+        content = image_file.read()
+        image = vision_client.image(content=content)
+    textz = image.detect_text()
+    return textz[0].description
+
+
+def decode_text(textz):
+    print(textz)
+
+
+def run():
+    raw_image = get_en_menu()
+    textz = text_from_image(raw_image)
+    result = decode_text(textz)
+    return result
+
--- chef/river.py
+++ chef/river.py
@@ -1,16 +1,15 @@
 import io
-import os
 import requests
 import re
 
 from google.cloud import vision
-
 
 def image_from_url(url):
     result = requests.get(url)
     if result.status_code != 200:
         raise ConnectionError()
     return result.content
+
 
 def text_from_image(raw_image):
     vision_client = vision.Client()
@@ -20,17 +19,20 @@ def text_from_image(raw_image):
     textz = image.detect_text()
     return textz[0].description
 
+
 def get_soup(meal_str):
     m = re.findall('[\d,]+l ([\w ,]+)', meal_str)
     return m[:-1]
+
 
 def get_meals(meal_str):
     m = re.findall('\d\. [\d]+g (.+?) (\d+)', meal_str, flags=re.DOTALL)
     return m
 
-def decode(menu):
-    soups = get_soup(menu)
-    meals = get_meals(menu)
+
+def decode_text(textz):
+    soups = get_soup(textz)
+    meals = get_meals(textz)
     print(soups)
     print(meals)
 
@@ -38,8 +40,8 @@ def decode(menu):
 def run():
     raw_image = image_from_url('http://www.campusriver.cz/images/19.tyden_free_2017b.jpg')
     textz = text_from_image(raw_image)
-    decode(textz)
-
+    result = decode_text(textz)
+    return result
 
 if __name__ == '__main__':
     run()
