a4586269d6c2e945a5fd0ff11553356ccb82431a
Tomas Zahradnik <tomas.zahradnik@kiwi.com>
2017-05-11 22:29:55 +0200

    add parsing for soups and meals

--- chef/river.py
+++ chef/river.py
@@ -1,6 +1,7 @@
 import io
 import os
 import requests
+import re
 
 from google.cloud import vision
 
@@ -19,9 +20,20 @@ def text_from_image(raw_image):
     textz = image.detect_text()
     return textz[0].description
 
+def get_soup(meal_str):
+    m = re.findall('[\d,]+l ([\w ,]+)', meal_str)
+    return m[:-1]
+
+def get_meals(meal_str):
+    m = re.findall('\d\. [\d]+g (.+?) (\d+)', meal_str, flags=re.DOTALL)
+    return m
+
 def decode(menu):
-    for l in menu.splitlines():
-        print(l)
+    soups = get_soup(menu)
+    meals = get_meals(menu)
+    print(soups)
+    print(meals)
+
 
 def run():
     raw_image = image_from_url('http://www.campusriver.cz/images/19.tyden_free_2017b.jpg')
