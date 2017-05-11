f1cb75a6df374d0000e1f49cc33e3927808bffd2
Martin Zaťko <martin.zatko@kiwi.com>
2017-05-11 21:44:16 +0200

    Added print function

--- chef/chef.py
+++ chef/chef.py
@@ -1,6 +1,6 @@
-
 import requests
 from . import settings
+
 
 def dummy_chef():
     msg = 'chef rulez'
@@ -10,3 +10,20 @@ def dummy_chef():
             'username': 'chef'})
     print(r.status_code)
     print(r.content)
+
+
+def build_post_string(restaurant):
+    post_string = ""
+    post_string += build_food_string(restaurant['Soup'])
+    post_string += build_food_string(restaurant['MainDish'])
+    return post_string
+
+
+def build_food_string(food_array):
+    food_string = ""
+    for food in food_array:
+        food_string += food['name']
+        food_string += " " + food['price']
+        food_string += " :carrot:" if food['is_vege'] else ""
+        food_string += "\n"
+    return food_string
