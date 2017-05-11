c07bf598ceb3ab6605d318be9131c52237375744
Michal Cab <michal.cab@kiwi.com>
2017-05-11 22:28:04 +0200

    chef: use velke sklizeno

--- chef/chef.py
+++ chef/chef.py
@@ -2,29 +2,33 @@ import math
 import requests
 from . import settings
 
+from sklizeno import VelkeSklizeno
+
 
 def dummy_chef():
-    msg = 'chef rulez'
+    msg = build_post_string(VelkeSklizeno().scrape().to_serializable())
     r = requests.post(settings.SLACK_WEBHOOK,
-    json = {'text': msg,
-            'channel':settings.SLACK_CHANNEL,
-            'username': 'chef'})
+                      json={
+                          'text': msg,
+                          'channel': settings.SLACK_CHANNEL,
+                          'username': 'chef'
+                      })
     print(r.status_code)
     print(r.content)
 
 
 def build_post_string(restaurant):
     post_string = ""
-    post_string += build_food_string(restaurant['Soup'])
-    post_string += build_food_string(restaurant['MainDish'])
+    post_string += build_food_string(restaurant['soups'])
+    post_string += build_food_string(restaurant['main_dishes'])
     return post_string
 
 
 def build_food_string(food_array):
     food_string = ""
     for food in food_array:
-        food_string += pretty_formater(food['name'])
-        food_string += " " + food['price']
+        food_string += pretty_formater(food['name'], food['price'])
+        food_string += " " + str(food['price'])
         food_string += " :carrot:" if food['is_vege'] else ""
         food_string += "\n"
     return food_string
@@ -41,7 +45,7 @@ def pretty_formater(food_name, food_price):
             else:
                 final_string += word
 
-    if len(final_string) + len(food_price) + 3 > 80:
+    if len(final_string) + len(str(food_price)) + 3 > 80:
         final_string += "\n"
 
     last_line = final_string.split("\n")
