fc995fe6ab29644d4e46ff3a0f665fc488b9ca77
Simone Esposito <chaufnet@gmail.com>
2018-05-16 10:32:15 +0200

    campus_river: Fallback to zomato&scraping

--- chef/restaurants/campus_river.py
+++ chef/restaurants/campus_river.py
@@ -3,10 +3,12 @@ import requests
 import datetime
 
 from .ocr import Ocr
+from .scraping import Scraping
+from .zomato import Zomato
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
-class CampusRiver(Ocr):
+class CampusRiverO(Ocr):
     index = 'http://www.campusriver.cz/index.php/poledni-menu'
 
     def get_english_menu_url(self):
@@ -28,6 +30,44 @@ class CampusRiver(Ocr):
         filtered_text = self.filter_image_text()
         return filtered_text[datetime.datetime.today().isoweekday()]
 
+class CampusRiverZ(Zomato):
+    def __init__(self):
+        self.zomato_id = 16507073
+
+    def get(self):
+        return super().get()[:5]
+
+    def filter(self, meal):
+        meal = re.sub('^[a-zA-Z0-9]\.', '', meal)  # menu number
+        meal = re.sub('[0-9,]+(g|l)', '', meal)  # size
+        meal = re.sub('[0-9]+,-', '', meal)  # price
+        meal = re.sub('\(A[0-9-,]+\)', '', meal)  # allergens
+        if meal.upper() == meal:  # title filter
+            return None
+        return meal
+
+class CampusRiverS(Scraping):
+    def __init__(self):
+        self.regex = 'nabidka_1[^>]+(?:><i)?>[^\s]+([^<]+)'
+        self.url = 'https://www.menicka.cz/3232-campus-river.html'
+        self.encoding = 'windows-1250'
+
+    def get(self):
+        ret = []
+        for i, food in enumerate(super().get()[:5]):
+            ret.append((food, {'veg': i == 4})) # Last item is vegetarian
+        return ret
+
+class CampusRiver:
+    @staticmethod
+    def get():
+        try:
+            return CampusRiverO().get()
+        except:
+            try:
+                return CampusRiverZ.get()
+            except:
+                return CampusRiverS.get()
 
 if __name__ == '__main__':
     print(CampusRiver().get())
